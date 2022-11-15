2ecc1dd219d092d7ada4a1fddd9d62a8305f3ece
michael lindsey <kennedyxiv@icloud.com>
2022-11-15 14:11:12 -0800

    Variables update

--- Variables/name.rb
+++ Variables/name.rb
@@ -1,4 +1,8 @@
-puts "What is your name? "
-user_name = gets.chomp
+puts "What is your first name? "
+first_name = gets.chomp
+puts "What is your last name? "
+last_name = gets.chomp
 
-puts "Hello #{user_name}, how are you doing tonight?"
+puts "Hello #{first_name} #{last_name}"
+
+10.times {puts "#{first_name} #{last_name}"}
